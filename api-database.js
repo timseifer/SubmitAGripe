85f4a49980cc4b5f85c3b095cd4b11d87e821998
Very-Squishy <79121156+Very-Squishy@users.noreply.github.com>
2021-05-04 16:54:58 -0400

    wtf mate

--- api-database.js
+++ api-database.js
@@ -180,8 +180,8 @@ theQuery = {submittedByUID: user_ID}
 				res.write("</div><div class=item2><div style= font-weight:bold>CATEGORY</div>"+items[i].GripeCategory);
 				res.write("<hr color=#000000></div><div class=item3><div style= font-weight:bold>VOTES</div><div id=votes>"+items[i].numVotes+"<hr color=#000000></div>");
 				res.write("</div><div class=item4><div style= font-weight:bold>DELETE POST</div>")
-				res.write("<input class=btn2 type="+'"'+"button"+'"'+"id="+'"' +items[i].submittedByUID+'"'+"name="+'"'+ items[i].GripeText+'"'+"onclick="+"deletion_button(this.id" + ','+"this.name)"+" value="+'"'+"DELETE"+'"'+" style="+'"'+"left: 50%; margin-left: -50px"+'"'+"><div>");	
-				res.write("<p><br></p><p> </p></div></div> </form> </div> <p></p>");		
+				res.write("<input class=btn2 type="+'"'+"button"+'"'+"id="+'"' +items[i].submittedByUID+'"'+"name="+'"'+ items[i].GripeText+'"'+"onclick="+"deletion_button(this.id" + ','+"this.name)"+" value="+'"'+"DELETE"+'"'+"><div>");	
+				res.write("<p><br></p></div></div> </form> </div> <p></p>");		
 			}			
 			res.end();
 		}
@@ -232,7 +232,7 @@ function User_Query_Everything(user_ID, res){
 			}
 			else{
 			
-			res.write("	<style> .myDiv { border: 2px outset black; text-align: center; border-radius: 10px; width: 95%; position:relative; left: 2%; display:block} .item1 { grid-area: header; } .item2 { grid-area: main; } .item3 { grid-area: right; }  .item4 {grid-area: footer} .grid-container {   display: grid;   grid-template-areas: 'header header header header' 'main main right right' 'footer footer footer footer'; border } .grid-container > div { text-align: center; } .btn2{   background-color: #f2994a;	width: 50px; 	font-size: 20px;	margin-left: -80px;	padding: 5px;border-radius: 10px;} .btn3{    background-color: #219653;	width: 50px; 	font-size: 20px; 	padding: 5px; 	margin-left: 30px;   color:#fff;    border-radius: 10px;}</style><p> </p>");
+			res.write("	<style> .myDiv { border: 2px outset black; text-align: center; border-radius: 10px; width: 95%; position:relative; left: 2%; display:block} .item1 { grid-area: header; } .item2 { grid-area: main; } .item3 { grid-area: right; }  .item4 {grid-area: footer} .grid-container {   display: grid;   grid-template-areas: 'header header header header' 'main main right right' 'footer footer footer footer'; border } .grid-container > div { text-align: center; } .btn2{   background-color: #f2994a;	width: 50px; 	font-size: 20px;	left:0%; margin-left: -80px;	padding: 5px;border-radius: 10px;} .btn3{    background-color: #219653;	width: 50px; 	font-size: 20px; 	padding: 5px; 	left:0%; margin-left: 30px;   color:#fff;    border-radius: 10px;}</style><p> </p>");
 				
 				for(i = 0; i< items.length; i++){
 					res.write("<p> </p><div class = myDiv style=" + '"' + "background: #56ccf2" + '">');
